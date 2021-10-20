ebedb25f83d98cbba405c7015f534f2041c2d51b
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-10-20 22:01:28 +0200

    Component Display with css reles added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import Info from './components/Info/info';
 import Keyboard from './components/Keyboard/Keyboard';
+import Display from './components/Actions/Display';
 
 function App() {
+  const phoneNumber = 667359961;
   return (
     <div className="container">
       <Info />
       <div className="keyboard-container">
         <Keyboard />
+      </div>
+      <div className="actions">
+        <Display phoneNumber={phoneNumber} />
       </div>
     </div>
   );
---
+++ src/components/Actions/Display.js
@@ -0,0 +1,7 @@
+const Display = ({ phoneNumber }) => {
+    return (
+        <span className="number">{phoneNumber}</span>
+    );
+}
+
+export default Display;
